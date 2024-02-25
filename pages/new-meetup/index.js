6e703c39d637cb86462e29fb79bdee1d707fefc9
Mariam Kvirkviia <marikanik1999@gmail.com>
2024-02-26 00:47:13 +0400

    add: 1 "head" Metadata 2 Deploying Next.js Projects

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,7 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
 export default function NewMeetupPage() {
   const router = useRouter();
   async function AddMeetup(data) {
@@ -14,6 +16,13 @@ export default function NewMeetupPage() {
 
     router.push("/");
   }
-
-  return <NewMeetupForm onAddMeetup={AddMeetup} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta name="description" content="Meetups" />
+      </Head>
+      <NewMeetupForm onAddMeetup={AddMeetup} />
+    </Fragment>
+  );
 }
